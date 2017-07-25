5f5340abafda157b74f7d9f5c878c83bd2196234
Natalie Dixon <natalie@nataliejdixon.com>
2017-07-25 21:27:23 +0100

    started project, adding package file, dependencies and gitignore

---
+++ app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+if (command === "add") {
+	notes.addNote(yargs.argv.title, yargs.argv-body);
+} else if (command === "remove") {
+	notes.getAll();
+} else if (command === "list") {
+	notes.removeNote(yargs.argv.title);
+} else if (command === "read") {
+	notes.readNote(yargs.argv.title);
+} else {
+	console.log("command not recognised");
+}
---
+++ notes.js
@@ -0,0 +1,23 @@
+var addNote = (title, body) => {
+
+}
+
+var removeNote = (title) => {
+	
+}
+
+var readNote = (title) => {
+	
+}
+
+var getAll = () => {
+
+}
+
+
+module.export = {
+	addNote,
+	removeNote,
+	getAll,
+	readNote
+}
