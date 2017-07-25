4a411eb42cf1dcb1798c8dd4dc67b21fa879685b
Natalie Dixon <natalie@nataliejdixon.com>
2017-07-25 22:01:12 +0100

    began adding addNote function

--- notes.js
+++ notes.js
@@ -1,17 +1,28 @@
-var addNote = (title, body) => {
+const fs = require('fs');
 
+var addNote = (title, body) => {
+	// create empty array to store all note objs in
+	var notes = [];
+	var newNote = {
+		title,
+		body
+	}
+	notes = JSON.parse(fs.readFileSync('notes-data.json'));
+	notes.push(newNote);
+	fs.writeFileSync('notes-data.json', JSON.stringify(newNote));
 }
 
 var removeNote = (title) => {
 	
-}
+}	
 
 var readNote = (title) => {
-	
-}
+	var openNote = fs.readFileSync('notes.json');
+}	openNote = JSON.parse(openNote);
 
 var getAll = () => {
-
+	var noteList = fs.readFileSync('notes.json');
+	console.log(JSON.stringify(noteList));
 }
 
 
